23d67b3149a165d267d2531b1fa085ac5f19be12
jeevanjagadish <jeev12597@gmail.com>
2020-07-10 17:00:55 +0530

    changes

---
+++ polltutorial/polls_one/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+
+from .models import Question,Choice
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    #list_display = ('question_text', 'pub_date', 'was_published_recently')
+    inlines = [ChoiceInline]
+admin.site.register(Question, QuestionAdmin)
+
+from .models import Choice, Question
+# ...
+admin.site.register(Choice)
---
+++ polltutorial/polls_one/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PollsOneConfig(AppConfig):
+    name = 'polls_one'
